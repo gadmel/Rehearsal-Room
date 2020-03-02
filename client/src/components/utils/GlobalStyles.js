2bd1f700483803b92e74071b83ae1ab957001ad2
Gleb Abramov <abramov.gleb@gmail.com>
2020-03-02 14:18:22 +0100

    Create GlobalStyles, install styled-components

---
+++ client/src/components/utils/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+* {
+  background: #135285;
+}
+
+body {
+  color: white;
+  display: flex;
+  margin: 0;
+  width: 100%;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    'PT Sans' sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 16px;
+}
+`
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import GlobalStyles from './components/utils/GlobalStyles'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <GlobalStyles>
+    <App />
+  </GlobalStyles>,
+  document.getElementById('root')
+)
