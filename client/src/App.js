84a86440f8974e8fcc1a45eb7c190e5f3bccec86
Gleb Abramov <abramov.gleb@gmail.com>
2020-03-02 14:12:05 +0100

    Create members/setMembers state, member list name

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  return <div>Rehearsal Room</div>
+  const [members, setMembers] = useState([
+    {
+      id: '001',
+      name: 'Viktor Miller',
+      band: 'Viktor Miller',
+      instruments: ['vocals', 'guitar'],
+      roles: ['musician'],
+      roomAsignment: 'RehearsalRoom_1',
+    },
+    {
+      id: '002',
+      name: 'Alexey Tietz',
+      band: 'XYNior',
+      instruments: ['vocals', 'guitar'],
+      roles: ['administrator', 'musician'],
+      roomAsignment: 'RehearsalRoom_1',
+    },
+  ])
+
+  return (
+    <div>
+      <header>Rehearsal Room</header>
+      {members.map(member => (
+        <article>{member.name}</article>
+      ))}
+    </div>
+  )
 }
 
 export default App
