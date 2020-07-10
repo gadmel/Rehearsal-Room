1203abcfbfd17f504b813aff4575fba52143545a
Gleb Abramov <abramov.gleb@gmail.com>
2020-07-10 23:15:49 +0200

    Correct forgotten font awesome unit implementation

--- client/src/components/utils/fontawesome.js
+++ client/src/components/utils/fontawesome.js
@@ -1,18 +1,20 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
-  faUsers,
-  faSignOutAlt,
-  faUserPlus,
-  faUserEdit,
-  faMinusCircle,
+   faUsers,
+   faSignOutAlt,
+   faUserPlus,
+   faUserEdit,
+   faMinusCircle,
+   faTimesCircle,
 } from '@fortawesome/free-solid-svg-icons'
 import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons'
 
 library.add(
-  faCalendarAlt,
-  faUsers,
-  faSignOutAlt,
-  faUserPlus,
-  faUserEdit,
-  faMinusCircle
+   faCalendarAlt,
+   faUsers,
+   faSignOutAlt,
+   faUserPlus,
+   faUserEdit,
+   faMinusCircle,
+   faTimesCircle
 )
